a84a74461abe93df27d2d4043b431d352c9443db
razzietheman <58360064+razzietheman@users.noreply.github.com>
2026-09-15 06:43:56 +0200

    Delete t9engine directory

--- t9engine/src/main/java/io/github/sspanak/tt9/preferences/settings/SettingsStore.java
+++
@@ -1,9 +0,0 @@
-package io.github.sspanak.tt9.preferences.settings;
-
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-
-public class SettingsStore extends SettingsStatic {
-	public SettingsStore(@NonNull Context context) { super(context); }
-}
